e85d01756763871b4eb8092042403f2dbc214f27
Daniel King <daniel.oliver.king@gmail.com>
2016-06-20 22:52:33 +0000

    Refactored parts 1 and 2, finished part 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -17,24 +17,12 @@ def copy_array arr
   result
 end
 
-def second_greatest arr
-  new_arr = copy_array arr
-  max_index = new_arr.index(new_arr.max)
-  new_arr.delete_at max_index
-  return new_arr.max
-end
-
 # Define a method max_2_sum(array) which takes an array of integers as an
 # argument and returns the sum of its two largest elements.
 # For an empty array it should return zero. 
 # For an array with just one element, it should return that element.
 def max_2_sum arr
-  return 0 if arr.empty?
-  return arr[0] if arr.length == 1
-  
-  max = arr.max
-  second = second_greatest arr
-  max + second
+  arr.empty? ? 0 : arr.sort {|x, y| y <=> x}.slice(0..1).reduce(:+)
 end
 
 # takes an array of integers and an additional integer, n, as arguments
@@ -43,7 +31,8 @@ end
 def sum_to_n? arr, n
   arr.each_with_index do |item_1, i1|
     arr.each_with_index do |item_2, i2|
-      return true if i1 != i2 && item_1 + item_2 == n
+      next if i1 == i2
+      return true if item_1 + item_2 == n
     end
   end
   false
@@ -70,7 +59,35 @@ def binary_multiple_of_4? s
 end
 
 # Part 3
+=begin
+Define a class BookInStock which represents a book with an ISBN number, isbn, 
+and price of the book as a floating-point number, price, as attributes. 
+Run associated tests via: $ rspec -e 'getters and setters' spec/part3_spec.rb
+The constructor should accept the ISBN number 
+(a string, since in real life ISBN numbers can begin with zero and can include hyphens) 
+as the first argument and price as second argument, and should raise ArgumentError 
+(one of Ruby's built-in exception types) if the ISBN number is the empty string 
+or if the price is less than or equal to zero. Include the proper getters and 
+setters for these attributes. Run associated tests via: $ rspec -e 'constructor' spec/part3_spec.rb
 
+Include a method price_as_string that returns the price of the book formatted 
+with a leading dollar sign and two decimal places, that is, a price of 20 should format as "$20.00" and a price of 33.8 should format as "$33.80".
+=end
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize isbn, price
+    raise ArgumentError if isbn == "" || price <= 0
+    
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    result = "$#{price.round(2)}"
+    until result =~ /\.\d\d$/ do
+      result += "0"
+    end
+    result
+  end
 end
